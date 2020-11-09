22330933610d6ff4a94051d32fe90bd2d9c0812a
Igor Isaev <igor.igorovich.isaev@gmail.com>
2020-11-09 08:52:32 +0200

    first

---
+++ HW_12_1.js
@@ -0,0 +1,21 @@
+// Задача_1_вариант_1
+
+for (var i = 0; i < 10; i++) {
+    (function(index) {
+        setTimeout(function() {
+            console.log(index);
+        }, 10);
+    }(i));
+}
+
+
+
+// Задача_1_вариант_2
+for(var i = 0; i < 10; i++) {
+    setTimeout((function(e) {
+        return function() {
+            console.log(e);
+        }
+    })(i), 10)
+}
+
--- HW_12_2.js
+++ HW_12_2.js
@@ -1,22 +1,3 @@
-// вариант_1
-
-for (var i = 0; i < 10; i++) {
-    (function(index) {
-        setTimeout(function() {
-            console.log(index);
-        }, 10);
-    }(i));
-}
-
-// вариант_2
-for(var i = 0; i < 10; i++) {
-    setTimeout((function(e) {
-        return function() {
-            console.log(e);
-        }
-    })(i), 10)
-}
-
 
 // задача_2
 const math = {
